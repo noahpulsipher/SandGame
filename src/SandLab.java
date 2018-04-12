8a3821cd1b163b3348669f38235e049f03b7856d
noahpulsipher <31518741+noahpulsipher@users.noreply.github.com>
2018-04-12 13:55:11 -0600

    added sand water metal and gas to interact.

--- src/SandLab.java
+++ src/SandLab.java
@@ -7,7 +7,11 @@ public class SandLab
   //add constants for particle types here
   public static final int EMPTY = 0;
   public static final int METAL = 1;
-  
+  public static final int SAND = 2;
+  public static final int WATER = 3;
+  public static final int GAS = 4;
+ 
+
   //do not add any more fields below
   private int[][] grid;
   private SandDisplay display;
@@ -23,13 +27,16 @@ public class SandLab
     String[] names;
     // Change this value to add more buttons
     //Step 4,6
-    names = new String[2];
+    names = new String[5];
     // Each value needs a name for the button
     names[EMPTY] = "Empty";
     names[METAL] = "Metal";
+    names[SAND] = "Sand";
+    names[WATER] = "Water";
+    names[GAS] = "Gas";
     
     //1. Add code to initialize the data member grid with same dimensions
-    
+    grid = new int[numRows][numCols];
     
     display = new SandDisplay("Falling Sand", numRows, numCols, names);
   }
@@ -38,15 +45,43 @@ public class SandLab
   private void locationClicked(int row, int col, int tool)
   {
     //2. Assign the values associated with the parameters to the grid
-   
+	grid[row][col] = tool;
   }
 
   //copies each element of grid into the display
   public void updateDisplay()
   {
+	  Color sand = new Color(235, 194, 136);
+	  Color water = new Color(44, 148, 178);
+	  Color gas = new Color(22, 206, 56);
       //Step 3
    //Hint - use a nested for loop
-    
+    for(int row = 0; row < grid.length; row++)
+    {
+    		for(int col = 0; col < grid[0].length; col++)
+    		{
+    			if(grid[row][col] == EMPTY)
+    			{
+    				display.setColor(row, col, Color.BLACK);
+    			}
+    			if(grid[row][col] == METAL)
+    			{
+    				display.setColor(row, col, Color.GRAY);
+    			}
+    			if(grid[row][col] == SAND)
+    			{
+    				display.setColor(row, col, sand);
+    			}
+    			if(grid[row][col] == WATER)
+    			{
+    				display.setColor(row, col, water);
+    			}
+    			if(grid[row][col] == GAS)
+    			{
+    				display.setColor(row, col, gas);
+    			}
+    		}
+    }
   }
 
   //Step 5,7
@@ -58,8 +93,64 @@ public class SandLab
     //The scalar refers to how big the value could be
     //int someRandom = (int) (Math.random() * scalar)
     //remember that you need to watch for the edges of the array
+	//No loops
+	  
+	  int randRow = (int) (Math.random() * (grid.length - 1));
+	  int randCol = (int) (Math.random() * (grid[0].length - 1));
+	  int randSide = (int) (Math.random() * 3);
     
-    
+	  if( ( randRow + 1 < grid.length) && grid[randRow][randCol] == SAND && (grid[randRow + 1][randCol] == EMPTY || grid[randRow + 1][randCol] == WATER))
+    		{	
+    			if(grid[randRow + 1][randCol] == WATER)
+    			{
+    				grid[randRow][randCol] = WATER;
+        			grid[randRow + 1][randCol] = SAND;
+    			}
+    			else
+    			{
+    				grid[randRow][randCol] = EMPTY;
+        			grid[randRow + 1][randCol] = SAND;
+    			}
+    		}
+	 
+	  if(grid[randRow][randCol] == WATER && randCol - 1 >= 0)
+	  {
+		  if(randSide == 1 && grid[randRow + 1][randCol] == EMPTY)
+		  {
+			  grid[randRow][randCol] = EMPTY;
+			  grid[randRow + 1][randCol] = WATER;
+		  }
+		  else if(randSide == 2 && grid[randRow][randCol - 1] == EMPTY)
+		  {
+			  grid[randRow][randCol] = EMPTY;
+			  grid[randRow][randCol - 1] = WATER;
+		  }
+		  else if(grid[randRow][randCol + 1] == EMPTY)
+		  {
+			  grid[randRow][randCol] = EMPTY;
+			  grid[randRow][randCol + 1] = WATER;
+		  }
+	  }
+	  
+	  if(grid[randRow][randCol] == GAS && randCol - 1 >= 0 && randRow - 1 >= 0)
+	  {
+		  if(randSide == 1 && grid[randRow - 1][randCol] == EMPTY)
+		  {
+			  grid[randRow][randCol] = EMPTY;
+			  grid[randRow - 1][randCol] = GAS;
+		  }
+		  else if(randSide == 2 && grid[randRow][randCol - 1] == EMPTY)
+		  {
+			  grid[randRow][randCol] = EMPTY;
+			  grid[randRow][randCol - 1] = GAS;
+		  }
+		  else if(grid[randRow][randCol + 1] == EMPTY)
+		  {
+			  grid[randRow][randCol] = EMPTY;
+			  grid[randRow][randCol + 1] = GAS;
+		  }
+	  }
+   
   }
   
   //do not modify this method!
